b30ba8dca24516cc184a5516435b250f9314ffc4
elgallegosph <mateosax1998@gmail.com>
2026-01-21 17:19:31 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Estos datos provienen de tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDea95aNqXhCuIOHPyrFwJKPX1sRAQBbEg",
+  authDomain: "elfutbolapp.firebaseapp.com",
+  projectId: "elfutbolapp",
+  storageBucket: "elfutbolapp.firebasestorage.app",
+  messagingSenderId: "68652520852",
+  appId: "1:68652520852:web:c6f274a48c57de05e5eb81"
+};
+
+// Inicializamos Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
